b6a534c09b17107c2896884ea71ead01169ac5f7
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-03-17 21:41:00 +0700

    Add files via upload

---
+++ CSPC-UPI/Kompetisi Latihan Dasar-Dasar Pemrograman 1/wilusump/wilusump.c
@@ -0,0 +1,15 @@
+/*
+	author 		: zicofarry/ Muhammad 'Azmi Salam
+	date solved	: 2024-09-09
+*/
+
+#include <stdio.h>
+int main(){
+	int n;
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++){
+		printf("Wilujeng Sumping.\n");
+	}
+
+	return 0;
+}
