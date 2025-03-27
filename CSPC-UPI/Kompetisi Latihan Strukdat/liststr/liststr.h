c5c5736118a14c450284a4a78a7f7062ec921ee9
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-03-27 09:32:10 +0700

    Add files via upload

---
+++ CSPC-UPI/Kompetisi Latihan Strukdat/liststr/liststr.h
@@ -0,0 +1,33 @@
+/*
+problem setter	: Rosa A. S.
+solved by 		: zicofarry/ Muhammad 'Azmi Salam
+date solved		: 2025-02-03
+*/
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+//declare macro
+#define slv solve();
+
+typedef struct{
+    // contents of the container
+    char str[101];
+}value;
+
+typedef struct elmt* alamatelmt;
+typedef struct elmt{
+    value container;
+    alamatelmt next;
+} element;
+
+typedef struct{
+    element* first;
+}list;
+
+// declare procedure
+void createList(list *L);
+void addFirst(value temp, list *L);
+void printElement(element *point);
+void printList(list L);
+void solve();
---
+++ CSPC-UPI/Kompetisi Latihan Strukdat/liststr/main.c
@@ -0,0 +1,3 @@
+#include "liststr.h" // include library "liststr.h"
+int main(){slv return 0;} // call macro slv and solve the program
+
