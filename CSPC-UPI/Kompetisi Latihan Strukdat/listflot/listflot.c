429ccbe19349aea0bdc723a5e794bc8576015343
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-03-31 02:04:31 +0700

    Add files via upload

---
+++ CSPC-UPI/Kompetisi Latihan Strukdat/listflot/listflot.c
@@ -0,0 +1,63 @@
+#include "listflot.h" // include header "listflot.h"
+
+// procedure to create the list
+void createList(list *L){
+    (*L).first = NULL;    
+}
+
+// procedure to add element to the first place
+void addFirst(value temp, list *L){
+    element* new;
+    new = (element*) malloc(sizeof(element));
+    new->container = temp;
+
+    // if list is empty    
+    if((*L).first == NULL){
+        new->next = NULL;
+    // if list is not empty
+    }else{
+        new->next = (*L).first;
+    }
+    (*L).first = new;
+    new = NULL;
+}
+
+// procedure to print the list
+void printElement(element *point){
+    printf("%.2f\n", point->container.num);
+}
+
+// procedure to print the list
+void printList(list L){
+    if(L.first != NULL){
+        /*if list is not empty*/
+        /*initialization*/
+        element* point = L.first;
+        int i = 1;
+        while(point != NULL){
+            /*process*/    
+            printElement(point);
+
+            /*iteration*/
+            point = point->next;
+            i++;
+        }
+    }else{
+        /*process is list if empty*/
+        printf("empty list\n");
+    }
+}
+
+// procedure to solve the problem
+void solve(){
+    list L; // declare list L
+    createList(&L); // create list L
+    int n; // declare the number of elements in the list
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){ // loop as many as n
+        value x; // declare string x
+        scanf("%f", &x.num); // scan float num
+        addFirst(x, &L); // addFirst string x to the list
+    }
+    printList(L); // print list
+}
