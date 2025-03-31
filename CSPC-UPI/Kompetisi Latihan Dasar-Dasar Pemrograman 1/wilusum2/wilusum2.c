e9d99bb1018f4c991dae3f91eeb90aad4764042c
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-04-01 06:17:25 +0700

    Add files via upload

---
+++ CSPC-UPI/Kompetisi Latihan Dasar-Dasar Pemrograman 1/wilusum2/wilusum2.c
@@ -0,0 +1,11 @@
+/*
+problem setter	: Rosa A. S. & M. Shalahuddin
+solved by 		: zicofarry/ Muhammad 'Azmi Salam
+date solved		: 2024-09-10
+*/
+#include <stdio.h>
+int main(){
+	int n; scanf("%d", &n); // deklarasikan dan input nilai n
+	for(int i = 0; i < n; i++) printf("Wilujeng Sumping,!\n"); // loop print sebanyak n
+	return 0;
+}
