94cbba2eeae66c4626c247b2f1a00c76c5680e89
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-07-12 04:33:20 +0700

    Add files via upload

---
+++ CSPC-UPI/Kompetisi Latihan Online Judge/colatoj4/colatoj4.c
@@ -0,0 +1,12 @@
+/*
+problem setter	: Rosa A. S.
+solved by 		: zicofarry/ Muhammad 'Azmi Salam
+date solved		: 2024-09-19
+*/
+#include <stdio.h>
+int main(){
+	int n; scanf("%d", &n); // declare and input n value
+	printf("%d\n", n); // print n value
+	printf("Coba Latihan Online Judge, Semangat!\n"); // print format
+	return 0;
+}
