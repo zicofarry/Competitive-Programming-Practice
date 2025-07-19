384899ecc7269625f646a4a1a3bd48eeed7f90b4
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-07-20 01:20:10 +0700

    Add files via upload

---
+++ CSPC-UPI/Kompetisi Latihan Dasar-Dasar Pemrograman 2/bilprima/bilprima.h
@@ -0,0 +1,12 @@
+/*
+problem setter	: Rosa A. S.
+solved by 		: zicofarry/ Muhammad 'Azmi Salam
+date solved		: 2025-01-18
+*/
+#include <stdio.h>
+#include <math.h>
+
+// deklarasi fungsi
+int prima(int n);
+int priboba(int n);
+int cari(int n);
