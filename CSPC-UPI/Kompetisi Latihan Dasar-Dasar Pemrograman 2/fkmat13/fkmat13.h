bf1dd5680f05fee5899b2069a76977c608f93042
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-08-03 21:21:49 +0700

    Add files via upload

---
+++ CSPC-UPI/Kompetisi Latihan Dasar-Dasar Pemrograman 2/fkmat13/fkmat13.h
@@ -0,0 +1,9 @@
+/*
+problem setter	: Rosa A. S.
+solved by 		: zicofarry/ Muhammad 'Azmi Salam
+date solved		: 2024-12-24
+*/
+#include <stdio.h>
+
+//fungsi rekursif untuk menghitung faktorial
+int faktorial(int n);
