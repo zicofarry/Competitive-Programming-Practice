bd14c7887f0cb75dd372aa0267197831a44bfcb8
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-08-05 05:46:06 +0700

    Add files via upload

---
+++ CSPC-UPI/Kompetisi Latihan Dasar-Dasar Pemrograman 2/fpbmat13/fpbmat13.h
@@ -0,0 +1,9 @@
+/*
+problem setter	: Rosa A. S.
+solved by 		: zicofarry/ Muhammad 'Azmi Salam
+date solved		: 2025-01-07
+*/
+#include <stdio.h>
+
+// mendeklarasikan fungsi fpb
+int fpb(int a, int m);
