34bfd48ca2d1bc2bcebf2c5281edd81db1888ec6
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-08-11 12:40:21 +0700

    Add files via upload

---
+++ CSPC-UPI/Kompetisi Latihan Dasar-Dasar Pemrograman 2/kihajar/kihajar.h
@@ -0,0 +1,20 @@
+/*
+problem setter	: Rosa A. S.
+solved by 		: zicofarry/ Muhammad 'Azmi Salam
+date solved		: 2025-01-19
+*/
+#include <bits/stdc++.h>
+#define DEBUG(str, var) cout << str << " = " << var << nl;
+#define nl "\n"
+#define vi vector<int>
+#define vs vector<string>
+#define slv solve();
+#define zicofarry                \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);
+using namespace std;
+
+// declare procedure and function
+int prima(int n);
+int cariPrima(int n);
+void solve();
