a735eb3df3899feed4e41c39437ee311c8d12bf8
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-08-22 00:21:17 +0700

    Add files via upload

---
+++ CSPC-UPI/Kompetisi Latihan Dasar-Dasar Pemrograman 2/pokoca13/pokoca13.h
@@ -0,0 +1,25 @@
+/*
+problem setter	: Rosa A. S.
+solved by 		: zicofarry/ Muhammad 'Azmi Salam
+date solved		: 2025-01-20
+*/
+#include <bits/stdc++.h>
+#define DEBUG(str, var) cout << str << " = " << var << nl;
+#define nl "\n"
+#define sp ' '
+#define dig0(n) setw(n) << setfill('0')
+#define vi vector<int>
+#define vs vector<string>
+#define sin(str)       \
+    string str;        \
+    cin >> str;
+#define din(n)         \
+    int n;             \
+    cin >> n;
+#define slv solve();
+#define zicofarry                \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);
+using namespace std;
+
+void solve();
