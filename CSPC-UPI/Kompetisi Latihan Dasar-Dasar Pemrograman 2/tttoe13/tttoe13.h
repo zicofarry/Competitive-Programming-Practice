ee72a68c3ff7f71fbc642ad966cf19a68ae06a2a
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-09-08 00:45:06 +0700

    Add files via upload

---
+++ CSPC-UPI/Kompetisi Latihan Dasar-Dasar Pemrograman 2/tttoe13/tttoe13.h
@@ -0,0 +1,14 @@
+/*
+problem setter	: Rosa A. S.
+solved by 		: zicofarry/ Muhammad 'Azmi Salam
+date solved		: 2025-01-18
+*/
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+    char nama[3], simbol;
+    int x, y;
+}tiktok;
+
+void menang(tiktok p[6]);
